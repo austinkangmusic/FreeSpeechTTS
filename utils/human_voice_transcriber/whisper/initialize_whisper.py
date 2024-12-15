9b5c3ccc8c3fbf894c84cca94eb0f203ab93ab75
austinkangmusic <austinkang.music@gmail.com>
2024-12-16 04:37:46 +0800

    Update initialize_whisper.py

--- utils/human_voice_transcriber/whisper/initialize_whisper.py
+++ utils/human_voice_transcriber/whisper/initialize_whisper.py
@@ -8,9 +8,12 @@ whisper_model_size = os.getenv('whisper_model_size')
 
 def initialize_whisper_model():
     device = get_device()  # Dynamically select device using cuda_device.py
-    model_size = f"{whisper_model_size}"
+    print(f"Loading '{whisper_model_size}' model...")
     
-    # Initialize Whisper model
-    whisper_model = WhisperModel(model_size, device=device, compute_type="float32")
+    # Provide the path to your local model directory
+    model_path = f"faster_whisper_models/{whisper_model_size}"
+    
+    # Initialize Whisper model with the local path
+    whisper_model = WhisperModel(model_path, device=device, compute_type="float32")
     
     return whisper_model
